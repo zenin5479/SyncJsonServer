5567edb59160abfdd7d9197e627afccaaec40661
zenin5479 <zenin5479@gmail.com>
2026-01-08 11:10:01 +0300

    Update 0.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -242,9 +242,9 @@ namespace SyncJsonServer
          }
          catch (HttpListenerException ex)
          {
-            Console.WriteLine(string.Format("Failed to start server: {0}", ex.Message));
+            Console.WriteLine("Failed to start server: {0}", ex.Message);
             Console.WriteLine("You may need to run as administrator or add URL ACL:");
-            Console.WriteLine($"netsh http add urlacl url=http://+:8080/ user={Environment.UserName}");
+            Console.WriteLine("netsh http add urlacl url=http://+:8080/ user={0}", Environment.UserName);
          }
          catch (Exception ex)
          {
