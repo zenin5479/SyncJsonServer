c0ec39cde05183329df9987cacf11cd228b7938b
zenin5479 <zenin5479@gmail.com>
2026-02-08 22:32:56 +0300

    Update 6.4 Обновлен метод. Проведен рефакторинг. Рабочий код.

--- Program.cs
+++ Program.cs
@@ -9,12 +9,12 @@ namespace SyncJsonServer
 {
    public class Item
    {
+      public DateTimeOffset Date { get; set; }
+      public long Timestamp { get; set; }
       public int Id { get; set; }
       public string Vendor { get; set; }
       public string Name { get; set; }
       public double Price { get; set; }
-      public DateTimeOffset Date { get; set; }
-      public long Timestamp { get; set; }
    }
 
    public class SyncServer
