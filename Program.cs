59a3b2c3a2c4eaa1c8e8f30656d1e46761c13ecf
zenin5479 <zenin5479@gmail.com>
2026-02-16 09:11:25 +0300

    Update 7.8 Обновлен метод. Проведен рефакторинг. Рабочий код.

--- Program.cs
+++ Program.cs
@@ -252,19 +252,7 @@ namespace SyncJsonServer
    {
       static void Main()
       {
-         SyncServer server = new SyncServer("http://127.0.0.1:8080/");
-         try
-         {
-            server.Start();
-         }
-         catch (HttpListenerException ex)
-         {
-            Console.WriteLine("Не удалось запустить сервер: {0}", ex.Message);
-         }
-         catch (Exception ex)
-         {
-            Console.WriteLine("Ошибка: {0}", ex.Message);
-         }
+         
       }
    }
 }
